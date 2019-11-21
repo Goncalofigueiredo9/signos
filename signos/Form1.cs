53e2734a474e74f4135c495b4ab607d400bce7f7
GoncaloF <GoncaloF@WIN-GESFLHLR4EE>
2019-11-21 11:50:25 +0000

    Adicionar arquivos de projeto.

---
+++ signos/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace signos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        signos dia = new signos();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            dia.recebe = textBox1.Text;
+            dia.recebe = textBox1.Text;
+            label1.Text = ("O dia da semana é " + dia.diadasemana);
+            label2.Text = dia.envia;
+            label3.Text = dia.signo;
+        }
+    }
+}
---
+++ signos/signos.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace signos
+{
+    class signos
+    {
+        private DateTime data;
+        public string recebe
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+            }
+        }
+
+        public string diadasemana
+        {
+            get
+            {
+                return data.ToString("dddddd");
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                DateTime hoje = DateTime.Now;
+                
+
+                int anos = hoje.Year - data.Year;
+                int mês = hoje.Month - data.Month;
+
+                if (mês < 0)
+                {
+                    anos--;
+                    mês += 12;
+                }
+
+                int dias = hoje.Day - data.Day;
+                if (mês < 0)
+                {
+
+                    mês--;
+                    dias += 30;
+                }
+
+                string frase = "A sua idade é ";
+                frase += anos.ToString() + " anos ";
+                frase += mês.ToString() + " meses ";
+                frase += dias.ToString() + " dias ";
+
+                return frase;
+            }
+        }
+
+        public string signo
+        {
+            get
+            {
+                DateTime hoje = DateTime.Now;
+
+                int anos = hoje.Year - data.Year;
+                int mês = hoje.Month - data.Month;
+                int dias = hoje.Day - data.Day;
+                string signoo = "O seu signo é";
+
+                if (mês <= 1)
+                {
+                    if (20 <=dias)
+                    {
+                        signoo += "";
+                    }
+                }
+
+                if(mês < 0)
+                {
+                    mês--;
+                    dias += 30;
+                }
+                return signoo;
+            }
+        }
+    }
+}
